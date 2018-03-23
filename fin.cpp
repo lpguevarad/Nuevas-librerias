47504206e8b368e772057e5ef16272b08462fd88
Leidy Guevara <lpguevarad@unal.edu.co>
2018-03-23 08:45:31 -0500

    agrego archivo fin, incompleto. Falta: Sumar los x. Version 2

--- fin.cpp
+++ fin.cpp
@@ -17,10 +17,17 @@ void read__numbers(std::string name)
   std::ifstream fin(name);
   int n;
   double x;
+  double suma(0);
+  for(x=1;x<=N;++x){
+    suma=
+  }
   while(fin) {
     fin >> n >> x;
     std::cout << n << "\t" << x << std::endl;
   }
+
+
+
   
   fin.close();
 
